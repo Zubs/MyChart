6ed58385503f5592cad74577117950886967dc81
Zubair Idris Aweda <zubairidrisaweda@gmail.com>
2020-10-01 00:47:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,9 +32,7 @@
 	</div>
 	<div class="card text-center">
  	    <div class="card-body">
-            	<h5 class="card-title">Special title treatment</h5>
-    		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    		<a href="#" class="btn btn-primary">Go somewhere</a>
+		    <p class="card-text"><i class="fa fa-bell"></i></p>
   	   </div>
   	   <div class="card-footer text-muted">
     		2020 &copy; Zubair Idris Aweda
